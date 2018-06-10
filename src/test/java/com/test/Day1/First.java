136485c9040e0aebbed3437863b81840f3fc506b
sztester <1009425440@qq.com>
2018-06-10 16:47:57 +0800

    the first commit

---
+++ src/test/java/com/test/Day1/First.java
@@ -0,0 +1,32 @@
+package com.test.Day1;
+
+import org.testng.annotations.*;
+
+public class First {
+    @BeforeTest
+    public void BeforeTest()
+    {
+        System.out.println("这个是BeforeTest的注解");
+    }
+    @BeforeMethod
+    public  void BeforeMethod()
+    {
+        System.out.println("这个是BeforeMethod的注解");
+    }
+    @Test
+    public void Test()
+    {
+        System.out.println("这个是Test的注解");
+    }
+    @AfterMethod
+    public void AfterMethod()
+    {
+        System.out.println("这个是AfterMethod的注解");
+    }
+    @AfterTest
+    public void AfterTest()
+    {
+        System.out.println("这个是AfterTest的注解");
+    }
+
+}
